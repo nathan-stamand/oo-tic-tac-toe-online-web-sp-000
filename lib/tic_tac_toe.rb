8b54e7288639e7c4a49d609ee92423a1847a3f2b
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-26 21:51:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,14 +74,6 @@ class TicTacToe
         if @board[combo[1]] == @board[combo[2]]
           if @board[combo[2]] == "X" || @board[combo[2]] == "O"
             combo
-          end
-        end
-      elsif full?
-        false
-      end
-    end
-  end 
-
   
   def full? 
     @board.none?(" ")
