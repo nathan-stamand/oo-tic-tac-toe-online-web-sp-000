9148163494ed5ed63fc1b6f08062b1f38ae95c89
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-26 20:50:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,8 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(input)
-    input.to_i - 1 
+  def self.input_to_index
+    self.to_i - 1 
   end
   end 
   
